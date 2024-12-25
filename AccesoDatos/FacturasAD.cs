d8b1f4d92d34c84d2a8c961503f1e09105032871
Lazarte, Hector <hlazarte@gmail.com>
2024-12-25 15:00:33 -0400

    Cambiar referencia a managed oracle client

--- AccesoDatos/FacturasAD.cs
+++ AccesoDatos/FacturasAD.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using facturasXML.EntidadNegocio;
-using Oracle.DataAccess.Client;
+using Oracle.ManagedDataAccess.Client;
 
 namespace facturasXML.AccesoDatos
 {
